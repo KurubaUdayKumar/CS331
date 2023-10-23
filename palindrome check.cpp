788a3e69f2fb422beb92062d3321a9640efabb26
Kuruba Uday Kumar <94357575+KurubaUdayKumar@users.noreply.github.com>
2023-10-23 15:33:37 +0530

    Add files via upload

---
+++ palindrome check.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool palin(string s, int i, int j)
+{
+    if (i>j) 
+        return true;
+    return (s[i]==s[j]) && palin(s, i+1, j-1);
+}
+int main()
+{
+    string s;
+    cin>>s;
+    if(palin(s, 0, s.length()-1))
+    cout<<"plaindrome";
+    else cout<<"not palindrome";
+}
